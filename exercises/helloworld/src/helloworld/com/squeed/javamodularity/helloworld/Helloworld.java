4dcafda2422f509d8c145e0e44b8486e9f21b46f
Martin Bäumer <martin.baeumer@gmail.com>
2019-03-24 15:13:25 +0100

    first commit

---
+++ exercises/helloworld/src/helloworld/com/squeed/javamodularity/helloworld/Helloworld.java
@@ -0,0 +1,7 @@
+package com.squeed.javamodularity.helloworld; 
+
+public class Helloworld{ 
+  public static void  main (String ... args){ 
+    System.out.println("Hello Modular World!" ); 
+  } 
+}
